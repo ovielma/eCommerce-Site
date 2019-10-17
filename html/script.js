c9bc5414aeb0dc0216fd0af561082e14b241276a
mjt70 <mjt70@txstate.edu>
2019-10-16 20:32:26 -0500

    added calculation functions and submission confirmation
    added functions that will auto calculate total, tax, and shipping. Also added a pop up window after user submits payment on the check out page.

--- html/script.js
+++ html/script.js
@@ -1,14 +1,40 @@
 
-/* function to calculate the sum of two products*/
-function sumFunction(p1, p2) {
-  document.write(p1 + p2);
+/* function to calculate the sum of 4 numbers*/
+function sumFunction(p1, p2, p3, p4) {
+	parseFloat(p1);parseFloat(p2);parseFloat(p3);parseFloat(p4);
+	var num = p1 + p2 + p3 + p4;
+	var n = num.toFixed(2);
+  return n;
 }
+/* function to calculate tax*/
+function tax(){
+	var num = productsObject.subtotal * .08
+	var n = num.toFixed(2);
+	return n;
+}
+/* function to calculate shipping*/
+function shipping(){
+	var num = productsObject.subtotal * .03;
+	var n = num.toFixed(2);
+	return n;
+}
+/*confirmation page for checkout page*/
+function submission() {
+  var myWindow = window.open("", "", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+  myWindow.document.write("<h1>Thank you for odering through us! We are working on your order.</h1>");
+  myWindow.document.write("<p>Your Order: </p>");
+  myWindow.document.write(productsObject.productOne);
+  myWindow.document.write("<br>");
+  myWindow.document.write(productsObject.productTwo);
+}
+
 /* object to hold product names, prices, and set functions*/
 var productsObject = {
   productOne:"iPhone 12",
   productTwo: "Snickers",
   priceOne: 1199.99,
   priceTwo: 5.99,
+  subtotal: 0.00,
   getProducts: function() {
     document.write(this.productOne + " " + this.productTwo);
   },
@@ -19,6 +45,9 @@ var productsObject = {
   setProductTwo: function(input, proprice) {
   	this.productTwo = input;
   	this.priceTwo = proprice;
+  },
+  setSubtotal: function(input) {
+  	this.subtotal = input;
   }
 }
 /*get functions to retrieve information from products object*/
@@ -26,11 +55,15 @@ function getProductOne(){
 	document.write(productsObject.productOne);
 }
 function getPriceOne(){
-	document.write(productsObject.priceOne);
+	return productsObject.priceOne;
 }
+document.getElementById("priOne").innerHTML = getPriceOne();
 function getProductTwo(){
 	document.write(productsObject.productTwo);
 }
 function getPriceTwo(){
-	document.write(productsObject.priceTwo);
+	return productsObject.priceTwo;
+}
+function getSubtotal(){
+	return productsObject.subtotal;
 }
