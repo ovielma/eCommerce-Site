d9775aed7bc92fc745fbd825e1d45c8b1971accd
Jason Castro <43423262+JasonCastroisme@users.noreply.github.com>
2019-10-18 02:11:40 -0500

    updated user info - fixed validation - added local storage variables

--- html/script.js
+++ html/script.js
@@ -2,28 +2,8 @@
 //********************************************/
 /*USER INFORMATION FUNCTIONS START - *Jason */
 
-/*class for later?
-class Customer {
-	constructor(fullname, address1, address2, city, state, zipcode, phoneNumber, emailname) {
-		this.fullname = fullname;
-		this.address1 = address1;
-		this.address2 = address2;
-		this.city = city;
-		this.state = state;
-		this.zipcode = zipcode;
-		this.phonenumber = phonenumber;
-		this.email = emailname;
-	}
-}*/
-
-
-//test function
-/*function display(){
-	alert("hello world");
-}*/
-
 function validatePhoneNumber(){
-	var phoneNumberInfo = document.getElementById("phoneNumberUserInfo").value;
+	var phoneNumberInfo = document.getElementById("pNumber").value;
 	var phoneNumberValidation = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
 	if( (phoneNumberInfo.match(phoneNumberValidation)))
 	{
@@ -32,6 +12,7 @@ function validatePhoneNumber(){
 	else
 	{
 		alert("Phone Number is Invalid Format. Required format = xxx-xxx-xxxx");
+		document.getElementById("userInfoForm").action = "UserInformation.html";
 		return false;
 	}
 }
@@ -46,13 +27,96 @@ function validateEmailAddress() {
       }
     else {
         alert("The Email Address is Invalid. Email Address Needs '@' symbol to be valid.");
+				document.getElementById("userInfoForm").action = "UserInformation.html";
         return false;
     }
 }
 
 function onClickUserInformation(){
+
+	var inputFullName = document.getElementById("uname");
+	localStorage.setItem("uname", inputFullName.value);
+
+	var inputAddress1 = document.getElementById("a1name");
+	localStorage.setItem("a1name", inputAddress1.value);
+
+	var inputAddress2 = document.getElementById("a2name");
+	localStorage.setItem("a2name", inputAddress2.value);
+
+	var inputCity = document.getElementById("ctyname");
+	localStorage.setItem("ctyname", inputCity.value);
+
+	var inputStateName = document.getElementById("myStates");
+	localStorage.setItem("myStates", inputStateName.value);
+
+	var inputZipCode = document.getElementById("zname");
+	localStorage.setItem("zname", inputZipCode.value);
+
+	var inputPhoneNumber = document.getElementById("pNumber");
+	localStorage.setItem("pNumber", inputPhoneNumber.value);
+
+	var inputEmail= document.getElementById("emailUserInfo");
+	localStorage.setItem("emailUserInfo", inputEmail.value);
+
 	validatePhoneNumber();
 	validateEmailAddress();
+}
+
+function sameAddress(){
+	var sameAddress1 = localStorage.getItem("a1name");
+	var sameAddress2 = localStorage.getItem("a2name");
+	var sameCity = localStorage.getItem("ctyname");
+	var sameState = localStorage.getItem("myStates");
+	var sameZipCode = localStorage.getItem("zname");
+
+	if (document.getElementById("checkbox-one").checked){
+		alert("populating shipping info from user information");
+    document.getElementById("address-1").value = sameAddress1;
+		document.getElementById("address-2").value = sameAddress2;
+		document.getElementById("city").value = sameCity;
+		document.getElementById("state").value = sameState;
+		document.getElementById("zip").value = sameZipCode;
+	}
+
+	if (document.getElementById("checkbox-two").checked){
+		document.getElementById("address-1").value = "";
+		document.getElementById("address-2").value = "";
+		document.getElementById("city").value = "";
+		document.getElementById("state").value = "";
+		document.getElementById("zip").value = "";
+		}
+
+}
+
+function cbChange(obj) {
+    var cbs = document.getElementsByClassName("cb");
+    for (var i = 0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+    }
+    obj.checked = true;
+		sameAddress();
+}
+
+function shippingSubmitFunction(){
+	var shippingAddress1 = document.getElementById("address-1");
+	localStorage.setItem("address-1", shippingAddress1.value);
+
+	var shippingAddress2 = document.getElementById("address-2");
+	localStorage.setItem("address-2", shippingtAddress2.value);
+
+	var shippingCity = document.getElementById("city");
+	localStorage.setItem("city", shippingCity.value);
+
+	var shippingStateName = document.getElementById("state");
+	localStorage.setItem("state", shippingStateName.value);
+
+	var shippingZipCode = document.getElementById("zip");
+	localStorage.setItem("zip", shippingZipCode.value);
+	document.getElementById("address-1").value = shippingAddress1;
+	document.getElementById("address-2").value = shippingAddress2;
+	document.getElementById("city").value = shippingCity;
+	document.getElementById("state").value = shippingState;
+	document.getElementById("zip").value = shippingZipCode;
 }
 //******************************************/
 /*USER INFORMATION FUNCTIONS END - *Jason */
@@ -85,6 +149,9 @@ function submission() {
   myWindow.document.write(productsObject.productOne);
   myWindow.document.write("<br>");
   myWindow.document.write(productsObject.productTwo);
+	myWindow.document.write("<br>");
+	//for starting to write summary submission of store
+	//myWindow.document.write(localStorage.getItem("address-1"));
 }
 
 /* object to hold product names, prices, and set functions*/
